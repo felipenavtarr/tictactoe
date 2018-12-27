7c17156e13ea77d1233837e59c33e7848a5f3bc9
felipenavtarr <felipenavtarr@gmail.com>
2018-12-27 18:58:41 +0100

    punto 3 (detectar ganador y mensaje) de p2p_mod4

--- src/app/game/header/header.component.ts
+++ src/app/game/header/header.component.ts
@@ -12,6 +12,7 @@ export class HeaderComponent implements OnInit {
 
   constructor(stateService:StateService) {
     this._stateService = stateService;
+    console.log("de header:" + this._stateService.state.isVictory);
    }
 
   ngOnInit() {
--- src/app/game/state.service.ts
+++ src/app/game/state.service.ts
@@ -4,7 +4,8 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 export interface State {
   turn: string,
   values: string[][],
-  movements: number
+  movements: number,
+  isVictory: boolean
 }
 
 @Injectable({
@@ -23,36 +24,64 @@ export class StateService {
 	      ['-','-','-'],
 	      ['-','-','-']
       ],
-      movements: 0
+      movements: 0,
+      isVictory: false
 	  };
 
 	  this._state$ = new BehaviorSubject(initialState);
-
   }
 
-  get state$ (): BehaviorSubject<State> {
+  get state$(): BehaviorSubject<State> {
     return this._state$; 
   }
 
-  get state (): State {
+  get state(): State {
     return this._state$.getValue();
   }
 
-  set state (state: State) {
+  set state(state: State) {
     this._state$.next(state);
   }
   
   updateValue(row, col) {
-    if(this.state.values[row][col] === '-') {
+    if(this.state.values[row][col] === '-' && !this.state.isVictory) {
       let newValue = this.state.turn === 'PLAYER X' ? 'X' : '0';
-      let newTurn = this.state.turn === 'PLAYER X' ? 'PLAYER 0' : 'PLAYER X';
       this.state.values[row][col] = newValue;
-      this.state.turn = newTurn;
       this.state.movements++;
+      this.state.isVictory = this.checkWinner(newValue);
+      if(!this.state.isVictory) {
+        let newTurn = this.state.turn === 'PLAYER X' ? 'PLAYER 0' : 'PLAYER X';
+        this.state.turn = newTurn;
+      }
       this._state$.next(this.state);
     }
   }
 
+  checkWinner(value: string) {
+    let board = this.state.values;
+
+    for (let i=0; i<3; i++) {
+      if(value === board[i][0] && board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
+        return true;
+      }
+    }
+
+    for (let i=0; i<3; i++) {
+      if(value === board[0][i] && board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
+        return true;
+      }
+    }
+
+    if(value === board[0][0] && board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
+      return true;
+    }
+
+    if(value === board[0][2] && board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
+      return true;
+    }
+
+    return false;
+  }
   
   reset() {
     this.state = {
@@ -62,7 +91,8 @@ export class StateService {
         ['-','-','-'],
         ['-','-','-']
       ],
-      movements: 0
+      movements: 0,
+      isVictory: false
     };
   }
 }
